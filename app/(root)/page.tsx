74b3da0d9966718570070f040c28414ea298981e
Marko Strba <mark3strba@gmail.com>
2025-05-17 18:44:55 +0200

    Setup Auth Layouts, Next Auth, OAuth with Google & GitHub Providers

---
+++ app/(root)/page.tsx
@@ -0,0 +1,26 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h1-bold">tailwind css if fun</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="ml-5">
-      <div className="h1-bold">tailwind css if fun</div>
-      <div className="h1-bold font-space-grotesk">tailwind css if fun</div>
-    </div>
-  );
-}
