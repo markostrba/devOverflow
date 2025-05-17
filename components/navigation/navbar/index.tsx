1a87c86b60b17accc3e5e58dfdbdc6d6709e337e
Marko Strba <mark3strba@gmail.com>
2025-05-17 21:24:04 +0200

    Add NavLinks and MobileNav

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
+import MobileNavigation from "./MobileNavigation";
 
 const NavBar = () => {
   return (
@@ -20,6 +21,8 @@ const NavBar = () => {
       <p>Global Search</p>
       <div className="flex-between gap-5">
         <Theme />
+
+        <MobileNavigation />
       </div>
     </nav>
   );
