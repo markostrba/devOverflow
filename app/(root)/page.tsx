34c76393658ef9824c24a956d0949ed8a88b9f60
mearko <76460780+markostrba@users.noreply.github.com>
2025-05-18 16:15:33 +0200

    Implement local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,40 @@
-import { auth, signOut } from "@/auth";
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
-export default async function Home() {
-  const session = await auth();
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
 
-  console.log(session);
-
+export default async function Home({ searchParams }: SearchParams) {
+  const { params = "" } = await searchParams;
   return (
     <>
-      <h1 className="h1-bold">tailwind css if fun</h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="cursor-pointer primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          className="flex-1"
+        />
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 }
