c8592439adf9d2637164a6dc8248e4b5ff138b93
mearko <76460780+markostrba@users.noreply.github.com>
2026-02-23 06:08:45 +0100

    Init

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "features/**/*.{js,jsx,ts,tsx}",
+    "lib/**/*.{js,ts}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/*.test.{js,jsx,ts,tsx}",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default config;
